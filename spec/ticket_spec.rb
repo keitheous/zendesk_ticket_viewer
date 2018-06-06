a934aaf96030dca9dae363579a988af68f2bda54
keitheous <keithchongwy@gmail.com>
2018-06-06 13:58:14 +1000

    Wrote spec for Ticket class

---
+++ spec/ticket_spec.rb
@@ -0,0 +1,20 @@
+require 'zendesk_ticket_viewer/ticket.rb'
+
+RSpec.describe Ticket do
+  describe '.initialize' do
+    it 'has an ID' do
+    end
+
+    it 'has a subject' do
+    end
+
+    it 'has a description' do
+    end
+
+    it 'has a created_at date' do
+    end
+
+    it 'has a creator' do
+    end
+  end
+end
