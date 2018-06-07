f011a25c8a62a52bfdcea133792fa471b790677a
keitheous <keithchongwy@gmail.com>
2018-06-07 18:08:26 +1000

    Refactored & Loosely couple ZendeskApi class

--- lib/zendesk_ticket_viewer/zendesk_api.rb
+++ lib/zendesk_ticket_viewer/zendesk_api.rb
@@ -1,12 +1,22 @@
 class ZendeskApi
   attr_reader :basic_auth
 
-  def initialize(basic_auth = nil)
-     @basic_auth = basic_auth || { :username => "keithchongwy@gmail.com", :password => 'spiceupyourlife' }
+  def initialize(auth_args = nil)
+     @basic_auth = authentication(auth_args)
    end
 
   def get_request(url = nil)
     HTTParty.get( url || 'https://keitheous.zendesk.com/api/v2/tickets.json',
       :basic_auth => basic_auth)
+  end
+
+  private
+
+  def authentication(auth_args)
+    auth_args || default_basic_auth
+  end
+
+  def default_basic_auth
+    { :username => "keithchongwy@gmail.com", :password => 'spiceupyourlife' }
   end
 end
