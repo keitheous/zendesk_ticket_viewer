2a04748708b4c2eaf8c78c75b207ed07aa188e9b
keitheous <keithchongwy@gmail.com>
2018-06-07 19:07:56 +1000

    Modified ZendeskApi classand improved its spec

--- lib/zendesk_ticket_viewer/zendesk_api.rb
+++ lib/zendesk_ticket_viewer/zendesk_api.rb
@@ -2,12 +2,14 @@ class ZendeskApi
   attr_reader :basic_auth
 
   def initialize(auth_args = nil)
-     @basic_auth = authentication(auth_args)
-   end
+    @basic_auth = authentication(auth_args)
+  end
 
-  def get_request(url = nil)
-    HTTParty.get( url || 'https://keitheous.zendesk.com/api/v2/tickets.json',
-      :basic_auth => basic_auth)
+  def get_request(url_args = {})
+    HTTParty.get(
+      url_builder(url_args[:url], url_args[:page]),
+      :basic_auth => basic_auth,
+    )
   end
 
   private
@@ -18,5 +20,19 @@ class ZendeskApi
 
   def default_basic_auth
     { :username => "keithchongwy@gmail.com", :password => 'spiceupyourlife' }
+  end
+
+  def url_builder(url, page)
+    return tickets_url + url_concat(page) if page
+
+    url || tickets_url
+  end
+
+  def tickets_url
+    'https://keitheous.zendesk.com/api/v2/tickets.json'
+  end
+
+  def url_concat(page_num)
+    "?page=#{page_num}"
   end
 end
--- spec/zendesk_api_spec.rb
+++ spec/zendesk_api_spec.rb
@@ -26,23 +26,42 @@ RSpec.describe ZendeskApi do
 
   describe '#get_request' do
     context "using default url, without a param" do
-      let(:default_response) { default_zendesk_api.get_request }
+      let(:zendesk_response) { default_zendesk_api.get_request }
 
-      it 'returns response for tickets' do
-        expect(default_response.keys).to eq(
-          ['tickets', 'next_page', 'previous_page', 'count']
+      it 'returns response with next_page url' do
+        expect(zendesk_response['next_page']).to eq(
+          'https://keitheous.zendesk.com/api/v2/tickets.json?page=2',
         )
+      end
+
+      it 'returns response with exactly 100 tickets on page 1 (max per page)' do
+        expect(zendesk_response['tickets'].size).to eq(100)
+      end
+
+      it 'returns response with count to indicate overall tickets sum' do
+        expect(zendesk_response['count']).to eq(104)
+      end
+    end
+
+    context "using default url, with page 2 as param" do
+      let(:zendesk_response_page_2) { default_zendesk_api.get_request(:page => 2) }
+
+      it 'returns response with next_page url' do
+        expect(zendesk_response_page_2['next_page']).to eq(nil)
+      end
+
+      it 'returns response with exactly 4 tickets on page 2' do
+        expect(zendesk_response_page_2['tickets'].size).to eq(4)
       end
     end
 
     context "using custom url (groups) as params" do
       let(:groups_url) { "https://keitheous.zendesk.com/api/v2/groups.json" }
-      let(:default_response) { default_zendesk_api.get_request(groups_url) }
+      let(:zendesk_response) { default_zendesk_api.get_request(:url => groups_url) }
 
       it 'returns response for groups' do
-        expect(default_response.keys).to eq(
-          ["groups", "next_page", "previous_page", "count"]
-        )
+        expect(zendesk_response.key?('groups')).to eq(true)
+        expect(zendesk_response.key?('tickets')).to eq(false)
       end
     end
   end
