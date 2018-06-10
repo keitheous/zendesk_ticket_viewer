0b4c8c0b529db762c7e13c8dbb928651f465b9c9
keitheous <keithchongwy@gmail.com>
2018-06-10 22:47:31 +1000

    Completed Cli#view_single_ticket

--- lib/zendesk_ticket_viewer/cli/cli.rb
+++ lib/zendesk_ticket_viewer/cli/cli.rb
@@ -12,7 +12,7 @@ class Cli
     loop do
       display_main_options
 
-      case user_input
+      case gets.chomp
       when 'e' then return exit_application
       when '1' then view_all_tickets
       when '2' then view_single_ticket
@@ -24,37 +24,54 @@ class Cli
   def view_all_tickets
     loop do
       display_all_tickets_options
-      choice = user_input
+      user_input = gets.chomp
 
-      return previous_menu if choice == 'e'
-      if valid_page_section?(choice.to_i)
-        display_page(choice.to_i)
+      return previous_menu if user_input == 'e'
+      if valid_page_section?(user_input.to_i)
+        tickets_viewer.display_page(user_input.to_i)
       else
         invalid_option
       end
     end
   end
 
-  def view_single_ticket(display_single_ticket = 'single ticket')
-    puts display_single_ticket
+  def view_single_ticket
+    loop do
+      display_single_ticket_options
+      user_input = gets.chomp
+
+      return previous_menu if user_input == 'e'
+      if valid_ticket?(user_input.to_i)
+        display_ticket(user_input.to_i)
+      else
+        invalid_option
+      end
+    end
   end
 
   private
 
-  def display_page(page)
-    tickets_viewer.display_page(page)
+  def display_ticket(ticket_id)
+    ticket = tickets[ticket_id]
+    puts [
+      "Ticket #{ticket.id}",
+      "Subject: #{ticket.subject}",
+      "Description: #{ticket.description}",
+      "Submitter : #{ticket.submitter}",
+      "created: #{ticket.created_at}"
+    ].unshift("\n").join("\n")
   end
 
   def max_pages
     tickets_viewer.page_sections.size
   end
 
-  def valid_page_section?(page_section)
-    (1..max_pages).cover?(page_section)
+  def valid_ticket?(ticket_id)
+    (1..tickets.size).cover?(ticket_id)
   end
 
-  def user_input
-    gets.chomp
+  def valid_page_section?(page_section)
+    (1..max_pages).cover?(page_section)
   end
 
   def display_main_options
@@ -75,7 +92,18 @@ class Cli
       'Zendesk Ticket Viewer: Viewing All Tickets by Page',
       'Options:',
       'Press (e) to Return to Previous Menu',
-      "Press (any number between 1-#{max_pages}) to View Pages",
+      "Press (page number between 1-#{max_pages}) to View Page",
+      '==============================================================='
+    ].unshift("\n").join("\n")
+  end
+
+  def display_single_ticket_options
+    puts [
+      '===============================================================',
+      'Zendesk Ticket Viewer: Viewing Single Ticket by Ticket ID',
+      'Options:',
+      'Press (e) to Return to Previous Menu',
+      "Press (ticket ID number between 1-#{tickets.size}) to View Ticket",
       '==============================================================='
     ].unshift("\n").join("\n")
   end
