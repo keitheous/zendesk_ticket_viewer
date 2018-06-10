108f499c3a6e5efbff1928b7948d86449f19b97f
keitheous <keithchongwy@gmail.com>
2018-06-10 23:33:13 +1000

    Refactored & Implemented DRY on Cli class

--- lib/zendesk_ticket_viewer/cli/cli.rb
+++ lib/zendesk_ticket_viewer/cli/cli.rb
@@ -3,46 +3,33 @@ require_relative 'tickets_viewer'
 class Cli
   attr_reader :tickets, :tickets_viewer
 
-  def initialize(tic_args, tic_viewer = TicketsViewer)
-    @tickets = tic_args
+  def initialize(tickets_hash, tic_viewer = TicketsViewer)
+    @tickets = tickets_hash
     @tickets_viewer = tic_viewer.new(tickets.values)
   end
 
   def run
     loop do
-      display_main_options
+      main_instructions
 
       case gets.chomp
       when 'e' then return exit_application
-      when '1' then view_all_tickets
-      when '2' then view_single_ticket
+      when '1' then view('all')
+      when '2' then view('single_ticket')
       else invalid_option
       end
     end
   end
 
-  def view_all_tickets
+  def view(mode)
     loop do
-      display_all_tickets_options
+      sub_options(mode)
       user_input = gets.chomp
 
       return previous_menu if user_input == 'e'
-      if valid_page_section?(user_input.to_i)
-        tickets_viewer.display_page(user_input.to_i)
-      else
-        invalid_option
-      end
-    end
-  end
 
-  def view_single_ticket
-    loop do
-      display_single_ticket_options
-      user_input = gets.chomp
-
-      return previous_menu if user_input == 'e'
-      if valid_ticket?(user_input.to_i)
-        display_ticket(user_input.to_i)
+      if valid_range?(user_input.to_i, max_range(mode))
+        display_content(mode, user_input.to_i)
       else
         invalid_option
       end
@@ -51,30 +38,31 @@ class Cli
 
   private
 
-  def display_ticket(ticket_id)
-    ticket = tickets[ticket_id]
-    puts [
-      "Ticket #{ticket.id}",
-      "Subject: #{ticket.subject}",
-      "Description: #{ticket.description}",
-      "Submitter : #{ticket.submitter}",
-      "created: #{ticket.created_at}"
-    ].unshift("\n").join("\n")
+  def max_range(mode)
+    mode == 'all' ? max_pages : tickets.size
+  end
+
+  def sub_options(mode)
+    mode == 'all' ? list_tickets_options : list_single_ticket_options
+  end
+
+  def display_content(mode, user_input)
+    mode == 'all' ? display_page(user_input) : display_ticket(user_input)
+  end
+
+  def display_page(page)
+    tickets_viewer.display_page(page)
   end
 
   def max_pages
     tickets_viewer.page_sections.size
   end
 
-  def valid_ticket?(ticket_id)
-    (1..tickets.size).cover?(ticket_id)
+  def valid_range?(user_input, max_range)
+    (1..max_range).cover?(user_input)
   end
 
-  def valid_page_section?(page_section)
-    (1..max_pages).cover?(page_section)
-  end
-
-  def display_main_options
+  def main_instructions
     puts [
       '===============================================================',
       'Zendesk Ticket Viewer: Main Menu',
@@ -86,7 +74,7 @@ class Cli
     ].unshift("\n").join("\n")
   end
 
-  def display_all_tickets_options
+  def list_tickets_options
     puts [
       '===============================================================',
       'Zendesk Ticket Viewer: Viewing All Tickets by Page',
@@ -97,7 +85,7 @@ class Cli
     ].unshift("\n").join("\n")
   end
 
-  def display_single_ticket_options
+  def list_single_ticket_options
     puts [
       '===============================================================',
       'Zendesk Ticket Viewer: Viewing Single Ticket by Ticket ID',
@@ -105,6 +93,18 @@ class Cli
       'Press (e) to Return to Previous Menu',
       "Press (ticket ID number between 1-#{tickets.size}) to View Ticket",
       '==============================================================='
+    ].unshift("\n").join("\n")
+  end
+
+  def display_ticket(ticket_id)
+    ticket = tickets[ticket_id]
+
+    puts [
+      "Ticket #{ticket.id} / #{tickets.size}",
+      "Subject: #{ticket.subject}",
+      "Submitter: #{ticket.submitter}",
+      "Created: #{ticket.created_at}",
+      "Description:\n#{ticket.description}"
     ].unshift("\n").join("\n")
   end
 
@@ -117,6 +117,10 @@ class Cli
   end
 
   def invalid_option
-    puts "Eeekkkk!! Invalid Option. Please read carefully and try again"
+    puts "Eeekkkk!! Invalid Option. Please read carefully and try again =)"
   end
 end
+
+# require_relative '../api_service_controller'
+# tickets = ApiServiceController.new.fetch_tickets.ticket_master.tickets
+# Cli.new(tickets).run
